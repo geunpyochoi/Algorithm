ce9aa7c56153600f979354af57e39ec76e9c8322
geunpyochoi <ckp220@kookmin.ac.kr>
2023-07-19 17:33:21 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,12 +1,4 @@
 function solution(a, b) {
     var answer = 0;
-    var ab = String(a)+String(b);
-    var ba = String(b)+String(a);
-    if (ab>ba){
-        answer = Number(ab);
-    }
-    else{
-        answer = Number(ba);
-    }
-    return answer;
+    return Math.max(String(a)+String(b),String(b)+String(a));
 }
