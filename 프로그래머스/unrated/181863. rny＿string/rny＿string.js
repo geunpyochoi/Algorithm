08213b3f75a555446a7780ea697d3c99c0684d82
geunpyochoi <ckp220@kookmin.ac.kr>
2023-07-24 17:57:40 +0900

    [unrated] Title: rny_string, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.js
@@ -0,0 +1,12 @@
+function solution(rny_string) {
+    var answer = '';
+    for(i = 0; i < rny_string.length; i++){
+        if(rny_string[i] == 'm'){
+            answer += 'rn';
+        }
+        else{
+            answer += rny_string[i];
+        }
+    }
+    return answer;
+}
