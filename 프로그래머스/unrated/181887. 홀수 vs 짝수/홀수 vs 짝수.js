a82f91843ae7e6bb5d1e8a93b0a45415454b1b77
geunpyochoi <ckp220@kookmin.ac.kr>
2023-08-22 23:26:56 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    var answer = 0;
+    let odd = 0;
+    let even = 0;
+    for(let i = 0; i < num_list.length; i++){
+        if (i % 2 === 0) odd += num_list[i];
+        else even += num_list[i];
+    }
+    return answer = odd > even ? odd : even;
+}
