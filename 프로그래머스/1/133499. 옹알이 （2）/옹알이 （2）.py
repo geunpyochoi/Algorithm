51c1090490854eb802cb9036ae2ed8ad9ced808a
geunpyochoi <ckp220@kookmin.ac.kr>
2023-10-13 18:09:38 +0900

    [level 1] Title: 옹알이 (2), Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,10 @@
+def solution(babbling):
+    answer = 0
+    w = ["aya", "ye", "woo", "ma"]
+    for i in babbling:
+        for j in w:
+            if j*2 not in i :
+                i = i.replace(j,' ')
+        if i.strip() == '':
+            answer += 1
+    return answer
